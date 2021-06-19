7ed638f9b97e06cb8aaf64abeac0495c3e76491f
Deeptimaan Banerjee 19BLC1162 <deeptimaan.banerjee2019@vitstudent.ac.in>
2021-06-19 23:56:38 +0530

    Improved UI and picture upload support

--- lsm/pdo.php
+++ lsm/pdo.php
@@ -1,4 +1,4 @@
 <?php
-$pdo = new PDO('mysql:host=localhost;port=3306;dbname=hartisans', 'han', 'han123');
+$pdo = new PDO('mysql:host=localhost;port=3306;dbname=hartisans', 'EnterUserName', 'EnterPassword');
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-$site='http://a4a19d947ca2.ngrok.io';
+$site='http://EnterSiteName.com';
